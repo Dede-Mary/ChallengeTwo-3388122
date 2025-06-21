666c9af102175ddce46a0acba2113e73dddcc799
Dede-Mary <dedemary102@gmail.com>
2025-06-20 19:37:07 -0800

    Add files via upload

---
+++ ch2.js
@@ -0,0 +1,40 @@
+
+/*new SearchSuggestionSystem(products)
+getSuggestions(searchWord)
+Input:
+products = ["mobile","mouse","moneypot","monitor","mousepad"]
+searchWord = "mouse"
+
+Output:
+[
+  ["mobile","moneypot","monitor"],
+  ["mobile","moneypot","monitor"],
+  ["mouse","mousepad"],
+  ["mouse","mousepad"],
+  ["mouse","mousepad"]
+]*/
+
+class SearchSuggestionSystem {
+  constructor(products) {
+    // Sort products for lexicographic order
+    this.products = products.sort();
+  }
+
+  getSuggestions(searchWord) {
+    let result = [];
+    let prefix = "";
+
+    for (let char of searchWord) {
+      prefix += char;
+
+      // Filter products with current prefix
+      const suggestions = this.products.filter(p => p.startsWith(prefix)).slice(0, 3);
+      result.push(suggestions);
+    }
+
+    return result;
+  }
+}
+const system = new SearchSuggestionSystem(["mobile","mouse","moneypot","monitor","mousepad"]);
+const output = system.getSuggestions("mouse");
+console.log(output);
